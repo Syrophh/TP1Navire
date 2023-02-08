2979a3671514c4a0493d101d64cd6c555fe6b62b
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-02-08 10:30:26 +0100

    Ajoutez des fichiers projet.

---
+++ TP1Navire/Navire.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1Navire
+{
+    class Navire
+    {
+        private string imo;
+        private string nom;
+        private string libelleFret;
+        private int qteFretMaxi;
+
+        public Navire(string imo, string nom, string libelleFret, int qteFretMaxi)
+        {
+            this.imo = imo;
+            this.nom = nom;
+            this.libelleFret = libelleFret;
+            this.qteFretMaxi = qteFretMaxi;
+        }
+        public Navire(string imo, string nom)
+        {
+            this.imo = imo;
+            this.nom = nom;
+            this.libelleFret = "Indéfini";
+            this.qteFretMaxi = 0;
+        }
+
+        public string Imo { get => imo; set => imo = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string LibelleFret { get => libelleFret; set => libelleFret = value; }
+        public int QteFretMaxi { get => qteFretMaxi; set => qteFretMaxi = value; }
+    }
+}
---
+++ TP1Navire/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TP1Navire
+{
+    class Program
+    {
+        static void Main()
+        {
+            TesterInstanciations();
+
+            Console.ReadKey();
+        }
+
+        public static void TesterInstanciations()
+        {
+            // déclaration de l'objet unNavire de la classe Navire
+            Navire unNavire;
+            // Instanciation de l'objet
+            unNavire = new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827);
+            // Declaration ET instanciation d'un autre objet de la classe Navire
+            Navire unAutreNavire = new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500);
+            Affiche(unNavire);
+            Affiche(unAutreNavire);
+            // ??
+            unAutreNavire = new Navire("IMO8715871", "MSC PILAR");
+            Affiche(unAutreNavire);
+        }
+
+        public static void Affiche(Navire navire)
+        {
+            Console.WriteLine($"Identification : {navire.Imo} ");
+            Console.WriteLine($"Nom :   {navire.Nom}");
+            Console.WriteLine($"Type de frêt :  {navire.LibelleFret}");
+            Console.WriteLine($"Quantité de Frêt :      {navire.QteFretMaxi}");
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
